6411cdb9ff337560a1015a6c5d3c6e78807b0140
Pablo Alcolea Sesse <128633352+PabloAlcoleaSesse@users.noreply.github.com>
2024-11-05 11:13:23 +0100

    Resolución del ejercicio 3

---
+++ Ejercicio_tres_Promedio /promedio.cpp
@@ -0,0 +1,22 @@
+//
+// Created by Pablo Alcolea Sesse on 5/11/24.
+//
+#include <iostream>
+#include "promedio.h"
+using namespace std;
+
+double promedio() {
+    int numero_notas = 10;
+    double notas_sum = 0;
+    double media;
+    int i;
+    for (int i = 0; i < numero_notas; i++) {
+        int nota;
+        std::cout << "Introduce la nota: ";
+        std::cin >> nota;
+        notas_sum += nota;
+    }
+    media = notas_sum/10;
+    cout << media << endl;
+    return media;
+}
--- main.cpp
+++ main.cpp
@@ -3,10 +3,15 @@
 //
 #include <iostream>
 #include "Ejercicio_uno_comparador/comparador.h"
+#include "Ejercicio_dos/semana.h"
+#include "Ejercicio_tres_Promedio /promedio.h"
+
 
 int main() {
 
     comparador();
+    asignador();
+    promedio();
     return 0;
 
 }
